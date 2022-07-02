9de1263ea8fb508a482e4cc1f81f593d473bb136
Ashley Gaterell <ash.gaterell@gmail.com>
2022-07-02 17:54:44 +1000

    Add playRound function

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 //create function named computerPlay that randomly chooses and returns either "Rock, Paper or Scissors"
 //create an array named choices
 //Randomly choose from the array and store in variable choice
-//Return computer selection.
+//Return computerselection.
 
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let computerSelection = Math.floor(Math.random() * choices.length)
     return choices[computerSelection]
 }
 
-console.log(computerPlay())
+//create a function named playRound that takes two parameters - playerSelection and computerSelection and then returns a string delcaring the winner.
+//if playerSelection equals computerSelection return tie
+//else if playerSelection equals "Rock" and computerSelection equals "Scissors" or 
+//playerSelection equals "Scissors" and computerSelection equals "Paper" or 
+//playerSelection equals "Paper" and computerSelection equals "Rock" then return player wins
+//else return player looses
+//create two const - playerSelection and computerSelection to pass to the function playRound
 
-//create function named PlayerPlay that request the user to input the choice of "Rock, Paper or Scissors" 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a TIE! Try again."
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+
+        return "You win this round, good on you!";
+    } else {
+        return "You loose :(";
+    }
+
+}
+
+const playerSelection = "paper"
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 
 
+//create function named game that plays through 5 rounds of the game and reports the winner at the end
 
-//create function named game that compares computerSelection and playerSelection and returns the winner. 
-//modify function game so that more than 1 game can be played 
 
