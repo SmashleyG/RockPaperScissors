5744bbfe75712192526f131f2c9f57065d499b8c
Ashley Gaterell <ash.gaterell@gmail.com>
2022-07-02 16:10:41 +1000

    Modify function and varible names

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //create function named computerPlay that randomly chooses and returns either "Rock, Paper or Scissors"
 //create an array named choices
 //Randomly choose from the array and store in variable choice
-//Return selected value.
+//Return computer selection.
 
-function computerSelection() {
+function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"];
-    let computerSelected = Math.floor(Math.random() * choices.length)
-    return choices[computerSelected]
+    let computerSelection = Math.floor(Math.random() * choices.length)
+    return choices[computerSelection]
 }
 
-console.log(computerSelection())
+console.log(computerPlay())
 
-//create function named PlayerPlay that request the user to input the choice of "Rock, Paper or Scissors"  
+//create function named PlayerPlay that request the user to input the choice of "Rock, Paper or Scissors" 
+
+
+
 //create function named game that compares computerSelection and playerSelection and returns the winner. 
 //modify function game so that more than 1 game can be played 
 
