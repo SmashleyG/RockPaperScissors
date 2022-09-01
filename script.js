ac197c6c89110ea167367244543fefed25ed278d
Ashley Gaterell <ash.gaterell@gmail.com>
2022-09-01 19:27:11 +1000

    Add menu gui

--- script.js
+++ script.js
@@ -53,3 +53,52 @@ function game() {
 
 //game();
 
+
+function displayMenu() {
+    const mainContainer = document.querySelector('#main-container');
+    mainContainer.style.width = '450px';
+    mainContainer.style.height = '650px';
+
+    // Title of game? 
+    const gameTitle = document.createElement('h1');
+    gameTitle.textContent = 'Rock Paper Scissors Game!';
+    gameTitle.id = 'game-title'
+    mainContainer.appendChild(gameTitle);
+
+
+    //Add menu container
+    const menu = document.createElement('div');
+    menu.id = 'menu';
+    mainContainer.appendChild(menu);
+
+    // Rules Image
+    const rules = document.createElement('img');
+    rules.src = 'images/Rules.png';
+    rules.id = 'rules-image';
+    menu.appendChild(rules);
+
+    // Instructions for choice of buttons
+    const menuInstruction = document.createElement('p');
+    menuInstruction.id = 'menu-instructions';
+    menuInstruction.textContent = 'Select the amount of Rounds per game.';
+    menu.appendChild(menuInstruction);
+
+    // Create buttons 
+
+    const roundOptions = [[1, "One"], [3, "Three"], [5, "Five"]];
+    roundOptions.forEach((menuRound) => {
+        let menuButton = document.createElement('button');
+        console.log(menuRound[0]);
+        if (menuRound[0] == 1) {
+            menuButton.textContent = `${menuRound[1]} Round`;
+        } else {
+            menuButton.textContent = `${menuRound[1]} Rounds`;
+        }
+        menuButton.id = `menu-${menuRound[1]}`
+        menuButton.classList = "menu-button";
+        menu.appendChild(menuButton);
+    });
+}
+
+
+displayMenu();
